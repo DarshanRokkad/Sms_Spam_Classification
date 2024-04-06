fa1bb97f814cd7d144fcaddce8c8cdc15945a64f
Darshan R.M <=>
2024-04-06 13:57:54 +0530

    pipeline updated

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -3,6 +3,7 @@ from src.logger import logging
 from src.exception import CustomException
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 
 
 class TrainingPipeline:
@@ -13,6 +14,14 @@ class TrainingPipeline:
             return train_data_path, test_data_path
         except Exception as e:
             raise CustomException(e, sys)
+        
+    def start_data_transformation(self, train_data_path:str, test_data_path:str):
+        try:
+            data_transformation = DataTransformation()
+            train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+            return train_arr, test_arr
+        except Exception as e:
+            raise CustomException(e, sys)
     
     def train_model(self):
         try:
@@ -20,6 +29,8 @@ class TrainingPipeline:
             
             logging.info('Data ingestion initiated')
             train_data_path, test_data_path = self.start_data_ingestion()
+            logging.info('Data transformation initiated')            
+            train_arr, test_arr = self.start_data_transformation(train_data_path, test_data_path)
             
             logging.info('Training pipeline completed')
         except Exception as e:
